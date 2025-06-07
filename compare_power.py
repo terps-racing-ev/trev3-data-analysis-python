b9b7fb4d5c999184a393f1ff479b601334ca3c63
Yasadu De Silva <49260501+lg4492@users.noreply.github.com>
2025-06-07 14:08:29 -0500

    Add files via upload

---
+++ compare_power.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+
+from data_pair import DataPair
+from data_file import DataFile
+
+# import the file
+rc_319 = DataFile("rc_319.csv")
+rc_318 = DataFile("rc_318.csv")
+
+# get voltage values from both
+pack_voltage_319 = rc_319.get_data_pair("PackVoltage")
+pack_voltage_318 = rc_318.get_data_pair("PackVoltage")
+pack_current_319 = rc_319.get_data_pair("PackCurrent")
+pack_current_318 = rc_318.get_data_pair("PackCurrent")
+
+# calculate power
+# this function will work even if the two columns are logged at different rates
+pack_power_318 = DataPair.merge_pairs(pack_voltage_318, pack_current_318, 
+                                      # multiplication op
+                                      lambda v, c: v * c,
+                                      # new label
+                                      "Power",
+                                      # new unit (matters for label)
+                                      "W")
+
+pack_power_319 = DataPair.merge_pairs(pack_voltage_319, pack_current_319, 
+                                      lambda v, c: v * c,
+                                      "Power",
+                                      "W")
+
+# shift the x axis so they both start at 0
+# (to overlay them instead of one after the other)
+zeroed_318 = pack_power_318.get_zeroed_x_axis_copy()
+zeroed_319 = pack_power_319.get_zeroed_x_axis_copy()
+
+# graph both in one 
+DataPair.graph_multiple([[zeroed_318, zeroed_319]], 
+                        # 318 will be blue, 319 will be red
+                        ['blue', 'red'])
+
+print("318 and 319 kWh")
+print(pack_power_318.integrate() / (3.6 * 1000000000))
+print(pack_power_319.integrate() / (3.6 * 1000000000))
+plt.show()
---
+++ graph_voltage_temp.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+
+from data_pair import DataPair
+from data_file import DataFile
+
+
+# import the file
+rc_319 = DataFile("rc_319.csv")
+rc_318 = DataFile("rc_318.csv")
+
+# get voltage values from both
+pack_voltage_319 = rc_319.get_data_pair("PackVoltage")
+pack_voltage_318 = rc_318.get_data_pair("PackVoltage")
+
+# get therm values from both
+pack_temp_319 = rc_319.get_data_pair("ThermAvg")
+pack_temp_318 = rc_318.get_data_pair("ThermAvg")
+
+# set units because spreadsheet doesn't specify them
+# units are just for labels
+pack_voltage_318.set_y_unit("V")
+pack_voltage_319.set_y_unit("V")
+pack_temp_318.set_y_unit("deg C")
+pack_temp_319.set_y_unit("deg C")
+
+# graph all 4 in one 
+DataPair.graph_multiple([[pack_voltage_318, pack_voltage_319], [pack_temp_318, pack_temp_319]], 
+                        # 318 will be blue, 319 will be red
+                        ['blue', 'red'], 
+                        # voltage will be solid line, temp will be dotted line
+                        ['-', ':'])
+
+plt.show()
